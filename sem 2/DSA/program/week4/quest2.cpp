c2fbf717c6f3e40f93e970bd51ff3c6cd321f3f5
saketraj10 <110519922+saketraj10@users.noreply.github.com>
2024-04-06 20:08:08 +0530

    Add files via upload

--- sem 2/DSA/program/week4/quest2.cpp
+++ sem 2/DSA/program/week4/quest2.cpp
@@ -1,106 +1,103 @@
-//Conversion of Infix expression to Postfix expression (using stack)  
-//Saket Raj
-//230970079
-//05-02-2024
 #include<iostream>
 #include<cstring>
+#include<cstdlib>
 using namespace std;
-class Stack{
-        private:
-        int count;
-        int size;
-        char* arr;
-        public:
-        Stack(int n){
-                size = n;
-                count = 0;
-                arr = new char[size];
-        }
-        bool isFull(){
-                if(count==size)
-                        return true;
-                else
-                        return false;
-        }
-        void push(char data){
-                if(isFull())
-                        cout<<"Stack is full, overflow"<<endl;
-                else
-                        arr[count++] = data;
-        }
 
-        bool empty(){
-                if(count==0)
-                        return true;
-                else
-                        return false;
+class Stack {
+private:
+    int count;
+    int size;
+    char* arr;
+public:
+    Stack(int n) {
+        size = n;
+        count = 0;
+        arr = new char[size];
+    }
+    bool isFull() {
+        return count == size;
+    }
+    void push(char data) {
+        if (isFull())
+            cout << "Stack is full, overflow" << endl;
+        else
+            arr[count++] = data;
+    }
+
+    bool empty() {
+        return count == 0;
+    }
+    void pop() {
+        if (empty())
+            cout << "Stack is empty, underflow" << endl;
+        else {
+            count--;
         }
-	    void pop(){
-            if(empty())
-                    cout<<"Stack is empty, underflow"<<endl;
-            else{
-                    count--;
-                }
-        }
-        char top(){
-            return arr[count-1];
-        }
+    }
+    char top() {
+        return arr[count - 1];
+    }
 
 };
-bool isOperand(char c){
-    return (c>='a' && c<='z')||(c>='A' && c<='Z')||(c>='0' && c<='9');
+
+bool isOperand(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
 }
 
-int precedence(char op){
-    if(op == '+' || op =='-') return 1;
-    if(op == '*' || op =='/') return 2;
-    if(op == '^') return 3;
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    if (op == '^') return 3;
     return 0;
 }
 
-void infixToPost(char infix[], char postfix[], int n){
-    Stack s(n+1);
+void infixToPost(char infix[], char postfix[], int n) {
+    Stack s(n + 1);
     int j = 0;
-    for(int i=0; infix[i]!='\0'; i++){
-        if(infix[i]==' ') continue;
-        if(isOperand(infix[i])){
+    for (int i = 0; infix[i] != '\0'; i++) {
+        if (infix[i] == ' ') continue;
+        if (isOperand(infix[i])) {
             postfix[j++] = infix[i];
+            postfix[j++] = ' '; 
         }
-        else if(infix[i]=='('){
+        else if (infix[i] == '(') {
             s.push(infix[i]);
         }
-        else if(infix[i]==')'){
-            while(!s.empty() && s.top()!='('){
+        else if (infix[i] == ')') {
+            while (!s.empty() && s.top() != '(') {
                 postfix[j++] = s.top();
+                postfix[j++] = ' '; 
                 s.pop();
             }
             s.pop();
         }
-        else{
-            while(!s.empty() && precedence(s.top())>=precedence(infix[i])){
-                postfix += s.top();
+        else {
+            while (!s.empty() && precedence(s.top()) >= precedence(infix[i])) {
+                postfix[j++] = s.top();
+                postfix[j++] = ' '; 
                 s.pop();
             }
             s.push(infix[i]);
         }
     }
 
-    while(!s.empty()){
+    while (!s.empty()) {
         postfix[j++] = s.top();
+        postfix[j++] = ' '; 
         s.pop();
     }
     postfix[j] = '\0';
 }
-int main(){
 
+int main() {
     char infix[100];
-    cout<<"Enter infix expression: ";
-    cin.get(infix, 100);
-    int n = strlen(infix);
-    char postfix[n+1];
+    cout << "Enter infix expression: ";
+    cin.getline(infix, 100);
+    int n = strlen(infix) + 1; 
+    char postfix[n];
 
     infixToPost(infix, postfix, n);
-    cout<<postfix<<endl;
-    
+    cout << postfix << endl;
+
     return 0;
 }
